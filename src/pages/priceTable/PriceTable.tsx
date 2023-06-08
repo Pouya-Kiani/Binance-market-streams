8995c62745669a45d16943ac7c2f095844c75aaa
huhu <p.kiani@live.com>
2023-06-08 19:27:19 +0330

    create PriceTable Page boilerplate

---
+++ src/pages/priceTable/PriceTable.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import { Wrapper } from './PriceTable.styled';
+
+export const PriceTable: FC = () => {
+  return (
+    <Wrapper>
+      <div className="">Price Table</div>
+    </Wrapper>
+  );
+};
