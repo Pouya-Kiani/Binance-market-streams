b1f9075ff5e1283790df9a7f6a8c465a7a019022
huhu <p.kiani@live.com>
2023-06-10 11:48:10 +0330

    create github pages deploy scripts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 3000 },
+  base: '/Binance-market-streams',
   plugins: [react()],
 });
