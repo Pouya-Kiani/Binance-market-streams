028bd1c4f73f1b7d5e667a2067c180bb1150631b
huhu <p.kiani@live.com>
2023-06-10 12:07:59 +0330

    update loading

--- src/pages/MarketPrices/MarketPriceTable.styled.ts
+++ src/pages/MarketPrices/MarketPriceTable.styled.ts
@@ -1,10 +1,10 @@
 import { styled } from 'styled-components';
 
 export const Wrapper = styled.main`
-  margin: 0;
   padding: 0;
   max-width: 500px;
   margin-inline: auto;
+  margin-block: auto;
   color: rgba(255, 255, 255, 0.87);
   background-color: #242424;
 `;
