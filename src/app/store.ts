ac2ee7587beb36454d89fb65873a459883a364c6
huhu <p.kiani@live.com>
2023-06-10 09:07:26 +0330

    add pricesApiSlice to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,13 +4,17 @@ import { persistStore } from 'redux-persist';
 
 // state slices:
 import { userReducer } from '../features/user';
+import pricesApiSlice from '../features/prices/pricesApiSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    [pricesApiSlice.reducerPath]: pricesApiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ serializableCheck: false }),
+    getDefaultMiddleware({ serializableCheck: false }).concat(
+      pricesApiSlice.middleware,
+    ),
 });
 setupListeners(store.dispatch);
 
